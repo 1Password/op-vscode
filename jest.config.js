b5cf9395daafdaa97886d9e30964b478368112f2
Jody Heavener <jody.heavener@agilebits.com>
2022-03-30 15:01:35 -0300

    Update test runner

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
 /** @type {import('@jest/types/build/Config').DefaultOptions} */
 module.exports = {
 	roots: ["<rootDir>"],
-	testMatch: ["<rootDir>/src/**/?(*.)+(test).[tj]s?(x)"],
+	testMatch: ["<rootDir>/src/**/?(*.)+(test).[tj]s"],
 	testEnvironment: "jest-environment-jsdom",
 	preset: "ts-jest/presets/js-with-ts",
 	transform: {
-		"^.+\\.(tsx|js)?$": "ts-jest",
+		"^.+\\.(ts|js)?$": "ts-jest",
 	},
-	testPathIgnorePatterns: [
-		"<rootDir>/dist",
-		"<rootDir>/node_modules",
-	]
+	testPathIgnorePatterns: ["<rootDir>/dist", "<rootDir>/node_modules"],
 };
