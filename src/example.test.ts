2ac7268120e4ec729277346b0b3caa11d4ba4e8b
Jody Heavener <jody.heavener@agilebits.com>
2022-04-12 11:29:59 -0300

    Set up vscode mock for jest tests, remove dummy test file

--- src/example.test.ts
+++
@@ -1,5 +0,0 @@
-// We need at least one test for CI to pass
-// Delete this once more tests are written
-test("example test", () => {
-	expect(1).toBe(1);
-});
---
+++ test/vscode-mock.ts
@@ -0,0 +1,3 @@
+export const window = {
+	createOutputChannel: () => jest.fn(),
+};
